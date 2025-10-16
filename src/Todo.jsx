b6ee6f0cf9a999f1e73be1826eff584de117d78a
Umasri537 <gubbalaumasri4@gmail.com>
2025-10-16 17:15:46 +0530

    Todolist Adv MethodsRefoctoring

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,11 +4,10 @@ function Todo(props) {
   return (
     <div className='border border-2 border-success-subtle m-3 p-3'>
         <li>
-        <span style={props.t.status ? {textDecoration:"line-through"}:{}}>{props.t.title}</span>
-        <button className='btn border-danger-subtle bg-danger-subtle m-2 p-2 ' onClick={()=>{props.deleteTodo(props.t.id)}}>Delete</button>
-        <button className={(props.t.status) ? "btn bg-secondary-subtle border-secondary-subtle":"btn bg-info-subtle border-info-subtle"} onClick={()=>{props.toggleTodo(props.t.id)}}>{(props.t.status) ? "undo" :"done"}</button>
-
-        </li>
+          <span style={props.t.status ? {textDecoration:"line-through"}:{}}>{props.t.title}</span>
+          <button className='btn border-danger-subtle bg-danger-subtle m-2 p-2 ' onClick={()=>{props.deleteTodo(props.t.id)}}>Delete</button>
+          <button className={(props.t.status) ? "btn bg-secondary-subtle border-secondary-subtle":"btn bg-info-subtle border-info-subtle"} onClick={()=>{props.toggleTodo(props.t.id)}}>{(props.t.status) ? "undo" :"done"}</button>
+       </li>
     </div>
   )
 }
