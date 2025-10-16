5b2152d6fe37c8f6ae24f63214c6a4b0f90e09ee
Umasri537 <gubbalaumasri4@gmail.com>
2025-10-16 16:18:43 +0530

    Todolist With DeleteButton

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -3,13 +3,18 @@ import Todo from './Todo'
 
 function Todolist() {
     var[todos,SetTodos]=React.useState(["Get Veggies","Join In Zym","Buy Groceries","Go To Trip","Pay Bills",])
+    function DeleteTodo(index){
+      var temp=[...todos];
+      temp.splice(index,1)
+      SetTodos([...temp]);
+    }
   return (
     <div className='border border-2 border-warning-subtle m-3 p-3'>
       <h2>Todolist</h2>
        <ul className='list-unstyled'>
         {
-            todos.map((todo)=>{
-               return <Todo t={todo}></Todo>
+            todos.map((todo,i)=>{
+               return <Todo t={todo} i={i} deleteTodo={DeleteTodo}></Todo>
             })
         }
        </ul>
